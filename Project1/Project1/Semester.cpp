d47e306e1a17595884d76c31f7bddb5c3c80f55b
HaoNhatt <88761759+HaoNhatt@users.noreply.github.com>
2022-03-13 23:01:27 +0700

    fix unwanted changed files

--- Project1/Project1/Semester.cpp
+++ Project1/Project1/Semester.cpp
@@ -5,3 +5,19 @@ using namespace std;
 #include "Semester.h"
 
 
+void editScoreBoard();
+void addNewStudent();
+void modifyStudentInfo();
+void enrollCourse();
+void inputFileClassInfo();
+void updateCourseInfo();
+void outputToScreenClassInfo();
+void exportStudentList();
+void importScoreBoard();
+void viewScourBoard();
+void updateResult();
+void viewCourseList();
+void deleteCourse();
+void createSchoolYear();
+void inputFileClassInfo();
+void outputToScreenClassInfo();
