dbdeba5f2995a4e7751385be1791ed5f2d7b3bda
HaoNhatt <haonhatht@gmail.com>
2022-04-14 21:51:13 +0700

    Update mainmenu UI

--- Project1/Project1/MainMenuStateTeacher.cpp
+++ Project1/Project1/MainMenuStateTeacher.cpp
@@ -1,5 +1,61 @@
 #include "MainMenuState.h"      
 
+void onTabSelected2(tgui::BackendGui& gui, tgui::String* curSelectedTab, vector<tgui::Group*>* vc, tgui::String selectedTab)
+{
+    //cerr << *curSelectedTab << '\n';
+    //cerr << selectedTab << '\n';
+
+    //cerr << vc->size() << '\n';
+
+    int selectedIndex = 0;
+    if (selectedTab == tgui::String("Courses Information")) {
+        *curSelectedTab = tgui::String("Courses Information");
+        selectedIndex = 0;
+    }
+    else if (selectedTab == tgui::String("Teacher Information"))
+    {
+        *curSelectedTab = tgui::String("Teacher Information");
+        selectedIndex = 1;
+    }
+
+    //cerr << selectedIndex << '\n';
+
+    for (int i = 0; i < (*vc).size(); i++)
+    {
+        (*vc)[i]->setVisible(false);
+    }
+
+    //(*vc)[1] = 5;
+    (*vc)[selectedIndex]->setVisible(true);
+}
+
+void onItemSelected2(tgui::Group& group_course, SchoolYear* schoolYears, Course* curCourse, tgui::String selectedItem) {
+    string sItem = selectedItem.toStdString();
+    bool check = false;
+    tgui::String courseInformation = "";
+
+    for (SchoolYear* i = schoolYears; i != nullptr && !check; i = i->nextSchoolYear) {
+        for (Semester* j = i->nowSemester; j != nullptr && !check; j = j->nextSemester) {
+            for (Course* k = j->nowCourse; k != nullptr && !check; k = k->nextCourse) {
+                if (!sItem.compare(k->courseName)) {
+                    check = true;
+                    curCourse = k;
+                    group_course.get<Label>("Course Name")->setTextSize(30);
+                    group_course.get<Label>("Course Name")->setText(k->courseName);
+                    group_course.get<Label>("Teacher Name")->setTextSize(13);
+                    group_course.get<Label>("Teacher Name")->setText(k->teacherName);
+                    courseInformation += '\n' + tgui::String(k->getFirstSessionDate()) + '\n' + '\n' + '\n';
+                    courseInformation += tgui::String(k->getSecondSessionDate()) + '\n';
+                    group_course.get<TextArea>("TextArea2")->setText(courseInformation);
+                    group_course.get<TextArea>("TextArea2")->setTextSize(30);
+                    continue;
+                    //cerr << k->teacherName << '\n';
+                }
+            }
+        }
+    }
+    showGroupCourse(group_course, check);
+}
 
 bool addComponents2(tgui::BackendGui& gui, SchoolYear*& schoolYears, tgui::String teacherName, tgui::Group& group_course,
     tgui::Group& group_student, tgui::Group& group_scoreboard, tgui::Group& group_studentInfo)
@@ -47,7 +103,7 @@ bool addComponents2(tgui::BackendGui& gui, SchoolYear*& schoolYears, tgui::Strin
     vc->push_back(&group_scoreboard);
     vc->push_back(&group_studentInfo);
     Course* curCourse = new Course;
-    group_course.get<Button>("Participants")->onClick(&onParticipants, ref(group_course), ref(curCourse));
+    group_course.get<Button>("Participants")->onClick(&onParticipants, ref(group_course), curCourse);
     group_course.get<tgui::TreeView>("TreeView1")->onItemSelect(&onItemSelected2, ref(group_course), schoolYears, curCourse);
     //group_student.get<tgui::Button>("ScoreBoard")->onClick(&onScoreboardSelected, ref(group_scoreboard), ref(group_student));
     //group_student.get<tgui::Button>("Student Info")->onClick(&onStudentInfoSelected, ref(group_studentInfo), ref(group_student));
@@ -80,14 +136,11 @@ void hideGroupCourseTeacher(Group& group_course)
     group_course.get<Label>("Course Name")->setVisible(false);
     group_course.get<Label>("Teacher Name")->setVisible(false);
     group_course.get<Label>("People")->setVisible(false);
-    // Date 
     group_course.get<Label>("Date")->setVisible(false);
-    group_course.get<Picture>("Picture3")->setVisible(false);
     group_course.get<TextArea>("TextArea2")->setVisible(false);
-
     group_course.get<Picture>("Picture1")->setVisible(false);
     group_course.get<Picture>("Picture2")->setVisible(false);
-
+    group_course.get<Picture>("Picture3")->setVisible(false);
     group_course.get<TextArea>("TextArea1")->setVisible(false);
     group_course.get<Button>("Participants")->setVisible(false);
     group_course.get<ListView>("PaList")->setVisible(false);
