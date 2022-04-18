289a8b81f5cebf064855100f5d05b6742c3e60da
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2022-04-19 00:58:36 +0700

    Changed second scoreboard mechanism

--- Project1/Project1/ClassScoreboard.cpp
+++ Project1/Project1/ClassScoreboard.cpp
@@ -4,7 +4,7 @@ map <tgui::String, vector<vector<tgui::String>>> mpa;
 map <tgui::String, vector<vector<tgui::String>> > mpb;
 
 void init_groupSB(Group& group_studentSB) {
-	group_studentSB.loadWidgetsFromFile("ClassScoreboardForm.txt");
+    group_studentSB.loadWidgetsFromFile("ClassScoreboardForm.txt");
 }
 
 void onStudentSBSelected(Group& group_studentSB, Group& group_student) {
@@ -79,7 +79,7 @@ void loadSBWidgets(Group& group_studentSB, SchoolYear*& schoolYear) {
     init_groupSB(group_studentSB);
 
     group_studentSB.get<tgui::ListView>("ListView1")->addColumn("Semester", 210, tgui::ListView::ColumnAlignment::Center);
-    group_studentSB.get<tgui::ListView>("ListView1")->addColumn("Courses", 210, tgui::ListView::ColumnAlignment::Center);
+    //group_studentSB.get<tgui::ListView>("ListView1")->addColumn("Courses", 210, tgui::ListView::ColumnAlignment::Center);
     group_studentSB.get<tgui::ListView>("ListView1")->addColumn("Class", 100, tgui::ListView::ColumnAlignment::Center);
     group_studentSB.get<tgui::ListView>("ListView1")->addColumn("StudentID", 110, tgui::ListView::ColumnAlignment::Center);
     group_studentSB.get<tgui::ListView>("ListView1")->addColumn("GPA", 100, tgui::ListView::ColumnAlignment::Center);
@@ -99,112 +99,100 @@ void loadSBWidgets(Group& group_studentSB, SchoolYear*& schoolYear) {
 
         tgui::String curDirectory = "SchoolYears/" + tgui::String(i->startYear) + "-" + tgui::String(i->endYear);
 
-        for (SpecificClass* j = i->nowClass; j; j = j->nextClass) {
-            group_studentSB.get<tgui::ComboBox>("ComboBox2")->addItem(tgui::String(j->classCODE));
+        for (SpecificClass* k = i->nowClass; k; k = k->nextClass) {
+            group_studentSB.get<tgui::ComboBox>("ComboBox2")->addItem(tgui::String(k->classCODE));
         }
 
         for (Semester* j = i->nowSemester; j; j = j->nextSemester) {
             curSemester--;
+
             group_studentSB.get<tgui::ComboBox>("ComboBox1")->addItem("Semester " + tgui::String(curSemester) + "/" + tgui::String(i->startYear) + "-" + tgui::String(i->endYear));
-            group_studentSB.get<tgui::ComboBox>("ComboBox1")->setSelectedItem("Semester " + tgui::String(curSemester) + "/" + tgui::String(i->startYear) + "-" + tgui::String(i->endYear));
+            //group_studentSB.get<tgui::ComboBox>("ComboBox1")->setSelectedItem("Semester " + tgui::String(curSemester) + "/" + tgui::String(i->startYear) + "-" + tgui::String(i->endYear));
             tgui::String curSemesterStr = "Semester " + tgui::String(curSemester);
             string curDir = curDirectory.toStdString() + "/" + "Semester" + (char)(curSemester + '0') + "/Courses/";
 
-            for (Course* k = j->nowCourse; k; k = k->nextCourse) {
+            for (SpecificClass* k = i->nowClass; k; k = k->nextClass) {
+                //group_studentSB.get<tgui::ComboBox>("ComboBox2")->addItem(tgui::String(k->classCODE));
 
-                //group_studentSB.get<tgui::ComboBox>("ComboBox1")->addItem(tgui::String(k->courseName));
-                string cName = curDir + k->courseName;
-                ifstream fin;
-                fin.open(cName + ".csv");
+                for (Student* curStudent = k->classStudent; curStudent; curStudent = curStudent->nextStudent) {
 
-                //cerr << cName + ".csv\n";
+                    //string strID = "";
+                    int GPA = 0;
+                    int cnt = 0, tmpID;
 
-                string line, word;
-                int num = 0;
-
-                while (getline(fin, line)) {
-                    num++;
-                    if (num == 1) {
-                        //cerr << "Lmao\n";
-                        continue;
-                    }
-                    
-                    stringstream str(line);
-                    int cnt = 0;
-
-                    int no = 0, stdID = 0, mTerm = 0, fMark = 0, oMark = 0, tMark = 0;
-                    char stdName[FULLNAMELENGTH];
-
-                    while (getline(str, word, ',')) {
+                    for (Course* curCourse = j->nowCourse; curCourse; curCourse = curCourse->nextCourse) {
                         cnt++;
-                        stringstream stoint(word);
-                        switch (cnt) {
-                        case 1:
-                            stoint >> no;
-                            break;
-                        case 2:
-                            stoint >> stdID;
-                            break;
-                        case 3:
-                            for (int i = 0; i < word.length(); i++) {
-                                stdName[i] = word[i];
+                        //group_studentSB.get<tgui::ComboBox>("ComboBox1")->addItem(tgui::String(k->courseName));
+                        string cName = curDir + curCourse->courseName;
+                        ifstream fin;
+                        fin.open(cName + ".csv");
+
+                        //cerr << cName + ".csv\n";
+
+                        string line, word;
+                        int num = 0;
+
+                        while (getline(fin, line)) {
+                            num++;
+                            if (num == 1) {
+                                //cerr << "Lmao\n";
+                                continue;
                             }
-                            stdName[word.length()] = '\0';
-                            break;
-                        case 4:
-                            stoint >> mTerm;
-                            break;
-                        case 5:
-                            stoint >> fMark;
-                            break;
-                        case 6:
-                            stoint >> oMark;
-                            break;
-                        case 7:
-                            stoint >> tMark;
-                            break;
-                        default:
-                            break;
+
+                            stringstream str(line);
+                            int cnt = 0;
+
+                            int no = 0, stdID = 0, mTerm = 0, fMark = 0, oMark = 0, tMark = 0;
+                            char stdName[FULLNAMELENGTH];
+
+                            while (getline(str, word, ',')) {
+                                cnt++;
+                                stringstream stoint(word);
+                                switch (cnt) {
+                                case 1:
+                                    stoint >> no;
+                                    break;
+                                case 2:
+                                    stoint >> stdID;
+                                    break;
+                                case 3:
+                                    for (int i = 0; i < word.length(); i++) {
+                                        stdName[i] = word[i];
+                                    }
+                                    stdName[word.length()] = '\0';
+                                    break;
+                                case 4:
+                                    stoint >> mTerm;
+                                    break;
+                                case 5:
+                                    stoint >> fMark;
+                                    break;
+                                case 6:
+                                    stoint >> oMark;
+                                    break;
+                                case 7:
+                                    stoint >> tMark;
+                                    break;
+                                default:
+                                    break;
+                                }
+                            }
+
+                            if (stdID == curStudent->studentID) GPA += tMark;
                         }
                     }
 
-                    tgui::String gpa = tgui::String(tMark);
-                    tgui::String className;
-
-                    string strID = "";
-                    int tmpID = stdID;
-                    while (tmpID) {
-                        strID += (char)(tmpID % 10 + '0');
-                        tmpID /= 10;
-                    }
-                    reverse(strID.begin(), strID.end());
-                    //cerr << strID << '\n';
-
-                    for (SpecificClass* sClass = i->nowClass; sClass; sClass = sClass->nextClass) {
-                        Student tmpStudent;
-                        string studentName;
-                        if (sClass->findStudent(strID, studentName, tmpStudent)) {
-                            className = tgui::String(sClass->classCODE);
-                            //cerr << "I can find it\n";
-                        }
-                    }
+                    GPA /= cnt;
 
                     vector<tgui::String> tmp;
-                    tgui::String item = k->courseName;
-                    for (Student* curStudent = k->nxtStudent; curStudent; curStudent = curStudent->nextStudent) {
-                        if (curStudent->studentID == stdID) {
-                            //cerr << curStudentName << '\n';
-                            int on4 = 4 * tMark / 10;
-                            //cerr << tgui::String(k->convertoABC(tMark));
-                            //cerr << item << ' ' << className << ' ' << tgui::String(curStudent->studentID) << ' ' << curStudentName << ' ' << gpa << ' ' << tgui::String(k->convertoABC(tMark)) << ' ' << tgui::String(on4) << '\n';
+                    int on4 = 4 * GPA / 10;
 
-                            tmp = { curSemesterStr + "/" + curYears, item, className, tgui::String(curStudent->studentID), gpa, gpa, tgui::String(k->convertoABC(tMark)), tgui::String(on4) };
-                            mpa[curSemesterStr + "/" + curYears].push_back(tmp);
-                            mpb[curSemesterStr + "/" + curYears + "/" + className].push_back(tmp);
-                            mpb["All Semesters/" + className].push_back(tmp);
-                            group_studentSB.get<tgui::ListView>("ListView1")->addItem(tmp);
-                        }
-                    }
+                    tmp = { tgui::String(curSemesterStr + "/" + curYears), tgui::String(k->classCODE), tgui::String(curStudent->studentID), tgui::String(GPA), tgui::String(GPA), tgui::String(j->nowCourse->convertoABC(GPA)), tgui::String(on4) };
+                    mpa[curSemesterStr + "/" + curYears].push_back(tmp);
+                    mpb[curSemesterStr + "/" + curYears + "/" + tgui::String(k->classCODE)].push_back(tmp);
+                    mpb[tgui::String("All Semesters/") + tgui::String(k->classCODE)].push_back(tmp);
+                    group_studentSB.get<tgui::ListView>("ListView1")->addItem(tmp);
+                    //tgui::String item = k->courseName;
                 }
             }
         }
